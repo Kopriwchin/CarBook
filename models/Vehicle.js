7368dc0be16849ccceec8874848c7f6acc60440a
Kopriwchin <iliqn1234a@gmail.com>
2025-12-27 03:13:55 +0200

    Add db models & tables

---
+++ models/Vehicle.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema({
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    brand: { type: String, required: true },
+    model: { type: String, required: true },
+    manufactureDate: { type: Date, required: true }, 
+    color: { type: String, required: true },
+    regPlate: { type: String, required: true, unique: true },
+    fuelType: { type: String, required: true },
+    insuranceExpiry: { type: Date },
+    hasFines: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Vehicle', vehicleSchema);
